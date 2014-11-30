d1be9b2d27945987873c2446871666022f247f20
Dennisbonke <dennisb@kabelfoon.net>
2014-11-30 19:59:26 +0100

    fixed a bit of the strawberrycrop, but it is not enabled

--- src/main/java/com/zandor300/advancedtools/blocks/crops/StrawberryCrop.java
+++ src/main/java/com/zandor300/advancedtools/blocks/crops/StrawberryCrop.java
@@ -2,9 +2,11 @@ package com.zandor300.advancedtools.blocks.crops;
 
 import javax.swing.Icon;
 
+import com.zandor300.advancedtools.reference.Reference;
 import net.minecraft.block.BlockCrops;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
 
 public class StrawberryCrop extends BlockCrops {
 
@@ -16,11 +18,11 @@ public class StrawberryCrop extends BlockCrops {
 		this.setBlockName("StrawberryCrop");
 	}
 
-	// @SideOnly(Side.CLIENT)
-	// public void registerIcons(IconRegister iconRegister) {
-	// this.iconArray = new Icon[4];
-	// for (int i = 0; i < this.iconArray.length; i++)
-	// this.iconArray[i] = iconRegister.registerIcons(RefStrings.MODID
-	// + ":tomato_" + i);
-	// }
+	 //@SideOnly(Side.CLIENT)
+	 //public void registerIcons(IIconRegister iconRegister) {
+	 //this.iconArray = new Icon[4];
+	 //for (int i = 0; i < this.iconArray.length; i++)
+	//	 this.iconArray[i] = (Icon) iconRegister.registerIcon(Reference.MOD_ID
+	//			 + ":tomato_" + i);
+	 //}
 }
