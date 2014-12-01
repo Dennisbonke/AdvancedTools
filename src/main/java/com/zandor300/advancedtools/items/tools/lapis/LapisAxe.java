730b403531f297c90709c5d7c592a533b796016a
Zandor300 <info@zsinfo.nl>
2014-12-01 22:04:38 +0100

    Preperation for Lapis tools.

---
+++ src/main/java/com/zandor300/advancedtools/items/tools/lapis/LapisAxe.java
@@ -0,0 +1,14 @@
+package com.zandor300.advancedtools.items.tools.lapis;
+
+import com.zandor300.advancedtools.items.genericitems.ATItemAxe;
+
+/**
+ * Created by Smith on 1-12-2014.
+ */
+public class LapisAxe extends ATItemAxe {
+
+    public LapisAxe(int id, ToolMaterial material) {
+        super(material);
+        this.setUnlocalizedName("LapisAxe");
+    }
+}
---
+++ src/main/java/com/zandor300/advancedtools/items/tools/lapis/LapisSword.java
@@ -0,0 +1,14 @@
+package com.zandor300.advancedtools.items.tools.lapis;
+
+import com.zandor300.advancedtools.items.genericitems.ATItemSword;
+
+/**
+ * Created by Smith on 1-12-2014.
+ */
+public class LapisSword extends ATItemSword {
+
+    public LapisSword(int id, ToolMaterial material) {
+        super(material);
+        this.setUnlocalizedName("LapisSword");
+    }
+}
