34617515f4cdbbbe0beb612267bba07f09affbcf
Dennisbonke <dennisb@kabelfoon.net>
2014-12-01 17:30:29 +0100

    Preparations for Enderion Tools

---
+++ src/main/java/com/zandor300/advancedtools/items/tools/enderion/EnderionHoe.java
@@ -0,0 +1,10 @@
+package com.zandor300.advancedtools.items.tools.enderion;
+
+import com.zandor300.advancedtools.items.genericitems.ATItemHoe;
+
+public class EnderionHoe extends ATItemHoe{
+    public EnderionHoe(int id, ToolMaterial material) {
+        super(material);
+        this.setUnlocalizedName("EnderionHoe");
+    }
+}
---
+++ src/main/java/com/zandor300/advancedtools/items/tools/enderion/EnderionSword.java
@@ -0,0 +1,10 @@
+package com.zandor300.advancedtools.items.tools.enderion;
+
+import com.zandor300.advancedtools.items.genericitems.ATItemSword;
+
+public class EnderionSword extends ATItemSword{
+    public EnderionSword(int id, ToolMaterial material) {
+        super(material);
+        this.setUnlocalizedName("EnderionSword");
+    }
+}
