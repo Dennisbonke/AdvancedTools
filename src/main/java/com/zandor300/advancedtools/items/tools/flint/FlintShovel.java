055c4d3b8a9a4e3bc248cb521bed4ba69fc6c69e
Dennisbonke <dennisb@kabelfoon.net>
2014-12-02 12:44:22 +0100

    prep for flint tools

---
+++ src/main/java/com/zandor300/advancedtools/items/tools/flint/FlintShovel.java
@@ -0,0 +1,10 @@
+package com.zandor300.advancedtools.items.tools.flint;
+
+import com.zandor300.advancedtools.items.genericitems.ATItemShovel;
+
+public class FlintShovel extends ATItemShovel{
+    public FlintShovel(int id, ToolMaterial material) {
+        super(material);
+        this.setUnlocalizedName("FlintShovel");
+    }
+}
