7c6f7fde274ac0794edd1a14a3548f5753cca3b6
Dennisbonke <dennisb@kabelfoon.net>
2014-12-03 10:50:03 +0100

    Wrapping up for release. version 0.5.0, the Spamming Git Edition.

--- src/main/java/com/zandor300/advancedtools/reference/Reference.java
+++ src/main/java/com/zandor300/advancedtools/reference/Reference.java
@@ -3,7 +3,7 @@ package com.zandor300.advancedtools.reference;
 public class Reference {
     public static final String MOD_ID = "advancedtools";
     public static final String MOD_NAME = "Advanced Tools";
-    public static final String VERSION = "1.7.10-0.4.0";
+    public static final String VERSION = "1.7.10-0.5.0";
     public static final String CLIENTSIDE = "com.zandor300.advancedtools.proxy.ClientProxy";
     public static final String SERVERSIDE = "com.zandor300.advancedtools.proxy.ServerProxy";
 }
